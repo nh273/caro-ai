d338c5e3b063161dd7a2fe364b4ccb9fc23b4185
Phillip Nguyen <nh273@cornell.edu>
2021-08-06 00:31:44 -0400

    adding docstrings

--- lib/game/game.py
+++ lib/game/game.py
@@ -19,8 +19,8 @@ class BaseGame(ABC):
     @abstractmethod
     def obs_shape(self) -> Tuple[int, ...]:
         """The shape of the neural network form of the game state.
-        This should be a tuple of ints. e.g. For a game of Tic-Tac-Toe it's
-        likely (2, 3, 3) i.e. the game state being fed into the neural net
+        This should be a tuple of ints. e.g. For a game of Tic-Tac-Toe it can be
+         (2, 3, 3) i.e. the game state being fed into the neural net
         is a 2x3x3 tensor: 2 player x 3x3 board (board viewed from each player's
         side)
         """
@@ -43,63 +43,73 @@ class BaseGame(ABC):
 
     @abstractmethod
     def possible_moves(self, mcts_state: int) -> List:
-        """[summary]
+        """Return list of all possible moves given the game state
 
         Args:
-            mcts_state (int): [description]
+            mcts_state (int): Game state in MCTS form
 
         Returns:
-            List: [description]
+            List: List of possible actions
         """
         pass
 
     @abstractmethod
     def invalid_moves(self, mcts_state: int) -> List:
-        """[summary]
+        """Return list of all invalid moves given game state.
+        Used to penalize these actions.
 
         Args:
-            mcts_state (int): [description]
+            mcts_state (int): Game state in MCTS form
 
         Returns:
-            List: [description]
+            List: List of invalid actions
         """
         pass
 
     @abstractmethod
     def states_to_training_batch(self, state_lists: List, who_moves_lists: List[int]) -> np.ndarray:
-        """[summary]
+        """Convert game states into a form that can be used as training data
+        for the neural net.
 
         Args:
-            state_lists (List): [description]
-            who_moves_lists (List[int]): [description]
+            state_lists (List): List of game states in MCTS form
+            who_moves_lists (List[int]): Corresponding list of player who made
+                the move at that game state.
 
         Returns:
-            np.ndarray: [description]
+            np.ndarray: For a 2 player game with board m x n, each game state
+            will be represented as a 2 x m x n array. Each array will reflect the
+            tokens/moves of one player with value 1 and have zero values in
+            all other locations (opposing player's token and empty locations).
+            This is how the data is fed to the neural network in the AlphaZero paper.
         """
         pass
 
     @abstractmethod
     def move(self, mcts_state: int, move: int, player: int) -> Tuple[int, bool]:
-        """[summary]
+        """Given the current game state and a (legal) move by a specific player,
+        get the new game state and whether or not the move led to a win.
 
         Args:
-            mcts_state (int): [description]
-            move (int): [description]
-            player (int): [description]
+            mcts_state (int): Game state in MCTS form
+            move (int): Action performed
+            player (int): Which of the 2 player (0 or 1) performed the action
 
         Returns:
-            Tuple[int, bool]: [description]
+            Tuple[int, bool]: New game state, whether the game had been won
+                (by the player who made the move)
         """
         pass
 
     @abstractmethod
     def render(self, mcts_state: int) -> Union[str, List[str]]:
-        """[summary]
+        """Render a string representation of the current game state
 
         Args:
-            mcts_state (int): [description]
+            mcts_state (int): Game state in MCTS form
 
         Returns:
-            Union[str, List[str]]: [description]
+            Union[str, List[str]]: String or list of str representation of
+            game state
         """
         pass
--- lib/game/tictactoe/tictactoe.py
+++ lib/game/tictactoe/tictactoe.py
@@ -7,15 +7,31 @@ Matrix = List[List[int]]
 
 
 class TicTacToe(BaseGame):
-    """Represents the Tic
+    """A representation of a subset of the general m,n,k game
+    (https://en.wikipedia.org/wiki/M,n,k-game)
+    with n=m and winning is by simply placing k tokens adjacent
+    to one another without any further conditions.
+    The game state can be represented in 2 ways:
+    - As a nxn list of lists of ints
+    where 1 & 0 represent tokens places by either player, and 2 represents
+    an empty square, a.k.a "Matrix" form
+    - As an nxn-digit integer, with the same meaning for 0, 1, and 2. The position
+    of each digit corresponding to the index of each square on the board from
+    top to bottom and left to right, e.g. on a 3x3 board:
+    |0|1|2|
+    |3|4|5|
+    |6|7|8|
     """
 
     def __init__(self, n: int = 3, k_to_win: int = 3):
-        """[summary]
+        """Create an instance of the game.
+        With default arguments the game is a Tic Tac Toe game.
 
         Args:
-            n (int, optional): [description]. Defaults to 3.
-            k_to_win (int, optional): [description]. Defaults to 3.
+            n (int, optional): Number of squares for each side of the game board.
+                Defaults to 3.
+            k_to_win (int, optional): Number of consecutive tokens to win.
+                Defaults to 3.
         """
         super().__init__()
         self.board_len = n
@@ -26,13 +42,13 @@ class TicTacToe(BaseGame):
 
     @staticmethod
     def flatten_nested_list(nested_list: List[List]) -> List:
-        """[summary]
+        """Unravel a double-nested list into a flat list
 
         Args:
-            nested_list (List[List]): [description]
+            nested_list (List[List])
 
         Returns:
-            List: [description]
+            List
         """
         return [item for sublist in nested_list for item in sublist]
 
@@ -70,37 +86,41 @@ class TicTacToe(BaseGame):
         return self.board_len ** 2
 
     def _pad_mcts_state(self, mcts_state_str: str) -> str:
-        """[summary]
+        """Since the game state in int form might have leading zeroes,
+        We have to pad it to proper length before converting to Matrix form
 
         Args:
-            mcts_state (int): [description]
+            mcts_state (int): Game state as int
 
         Returns:
-            str: [description]
+            str: String of game state in MCTS-friendly form, padded
+            to appropriate length with leading zeroes
         """
         return mcts_state_str.rjust(self.board_len ** 2, "0")
 
-    def encode_game_state(self, state_list: List[List[int]]) -> int:
-        """[summary]
+    def encode_game_state(self, state_list: Matrix) -> int:
+        """Convert game state from Matrix form to MCTS-friendly form (int)
+        which is smaller to store and hashable for quick looking in MCTS search
 
         Args:
-            state_list (List[List[int]]): [description]
+            state_list (Matrix): Game state as list of lists of tokens
 
         Returns:
-            int: [description]
+            int: Game state as int
         """
         flattened = self.flatten_nested_list(state_list)
         stringified = [str(i) for i in flattened]
         return int(''.join(stringified))
 
     def convert_mcts_state_to_list_state(self, mcts_state: int) -> Matrix:
-        """[summary]
+        """Convert game state from more compact, hashable i.e. MCTS-friendly form
+        to a form that is easier for us to think about (matrix of tokens)
 
         Args:
-            mcts_state (Hashable): [description]
+            mcts_state (int): Game state in MCTS-friendly form
 
         Returns:
-            (Matrix): [description]
+            (Matrix): Game state in more human-friendly form (list of list of tokens)
         """
         # Pad to number of squares on board (in case of leading zeros)
         padded = self._pad_mcts_state(str(mcts_state))
@@ -114,10 +134,13 @@ class TicTacToe(BaseGame):
         return state
 
     def possible_moves(self, mcts_state: int) -> List:
-        """[summary]
+        """Return indexes of empty squares, left to right, top to bottom
+        |0|1|2|
+        |3|4|5|
+        |6|7|8|
 
         Args:
-            mcts_state (Hashable): [description]
+            mcts_state (int): Game state in MCTS-friendly form
 
         Returns:
             Iterable: [description]
@@ -126,13 +149,13 @@ class TicTacToe(BaseGame):
         return [i for i, c in enumerate(padded) if c == str(self.empty)]
 
     def invalid_moves(self, mcts_state: int) -> List:
-        """[summary]
+        """Return the non-empty squares
 
         Args:
-            mcts_state (Hashable): [description]
+            mcts_state (int): Game state in MCTS-friendly form
 
         Returns:
-            List: [description]
+            List: List of illegal moves (occupied squares)
         """
         padded = self._pad_mcts_state(str(mcts_state))
         return [i for i, c in enumerate(padded) if c != str(self.empty)]
@@ -140,9 +163,8 @@ class TicTacToe(BaseGame):
     def _encode_list_state(self, dest_np: np.ndarray, state: Matrix, who_move: int) -> None:
         """
         In-place encodes list state into the zero numpy array
-        :param dest_np: dest array, expected to be zero
-        :param state_list: state of the game in the list form
-        :param who_move: player index (game.PLAYER_WHITE or game.PLAYER_BLACK) who to move
+        :param dest_np: dest array, expected to be zeroes
+        :param who_move: player index (game.player_white or game.player_black)
         """
         assert dest_np.shape == self.obs_shape
 
@@ -155,14 +177,27 @@ class TicTacToe(BaseGame):
 
     def states_to_training_batch(self, state_ints: List[int],
                                  who_moves_lists: List[int]) -> np.ndarray:
-        """[summary]
+        """Convert game states to arrays that can be fed to neural network
 
         Args:
-            state_ints (List[int]): [description]
-            who_moves_lists (List[int]): [description]
+            state_ints (List[int]): List of game states in MCTS form
+            who_moves_lists (List[int]): Corresponding list of player whose move
+                led to the game state
 
         Returns:
-            List[List]: [description]
+            np.array:  each game state will be represented as a
+            2 x board_len x board_len array. Each array will reflect the
+            tokens/moves of one player with value 1 and have zero values in
+            all other locations (opposing player's token and empty locations).
+            This is how the data is fed to the neural network in the AlphaZero paper.
+            e.g.:
+            |0|1|
+            | |0| with player 0 becomes:
+
+            [[[1, 0],
+              [0, 1]],
+             [[0, 1],
+              [0, 0]]]
         """
         batch_size = len(state_ints)
         batch = np.zeros((batch_size,) + self.obs_shape, dtype=np.float32)
@@ -172,15 +207,20 @@ class TicTacToe(BaseGame):
         return batch
 
     def move(self, mcts_state: int, move: int, player: int) -> Tuple[int, bool]:
-        """[summary]
+        """At a given game state, make a (legal) move by a specified player
 
         Args:
-            mcts_state (int): [description]
-            move (int): [description]
-            player (int): [description]
+            mcts_state (int): Game state in MCTS form
+            move (int): Location of the move, can be calculated as index of square
+                on the board from top to bottom, right to left e.g.
+                |0|1|2|
+                |3|4|5|
+                |6|7|8|
+            player (int): 0 or 1, which player is making the move
 
         Returns:
-            Tuple[int, bool]: [description]
+            Tuple[int, bool]: New game state & if the game had been won by the
+            player who just made the move
         """
         assert player == self.player_white or player == self.player_black
         assert move >= 0 and move <= self.action_space
@@ -194,12 +234,12 @@ class TicTacToe(BaseGame):
         return new_mcts_state, won
 
     def render(self, mcts_state: int) -> str:
-        """[summary]
+        """String representation of board, for interacting with human player
 
         Args:
-            mcts_state (int): [description]
+            mcts_state (int): Game state in MCTS form
 
         Returns:
-            str: [description]
+            str: String representation of game state
         """
         pass
